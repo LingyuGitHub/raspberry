23ddb06f9b7ca7ca0c6d3c046e4f42db83a95cf0
ly <lingyuwhu@outlook.com>
2018-11-16 20:23:59 +0800

    **

--- car/main.cpp
+++ car/main.cpp
@@ -1,63 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 #ifdef __cplusplus
 extern "C" {
 #include <wiringPi.h>
 }
 #endif
-#include "callservo.h"
-#include "msensor.h"
-#include "mcomn.h"
-#include "wheel.h"
+#include "tcpserver.hpp"
+#include "include/l298n.hpp"
 
-int state=STOP;
+using namespace std;
 
-static void showdis(double dis){
-	char str[100]={0};
-	sprintf(str,"dis=%.3lf", dis);
-	
-	if(dis<=0.5 and state==FORWARD){
-		control_wheel(0, STOP);
-	}
+
+void onrecv(const char *msg)
+{
+
 }
 
 int main(){
 	int wheel_pins[4]={0,1,2,3};
-	int echo_pins[2]={4,5};
-	char c=' ';
 
 	wiringPiSetup();
 
-	if(addpins(wheel_pins[0], wheel_pins[1],wheel_pins[2],wheel_pins[3])==-1){
-		return -1;
+	L298N wheel(0,1,2,3);
+
+	while (1)
+	{
+		wheel.goFore();
+		delay(4000);
+		wheel.turnLeft();
+		delay(4000);
+		wheel.turnRight();
+		delay(4000);
+		wheel.goBack();
+		delay(4000);
 	}
 
-	echo_setup(echo_pins[0], echo_pins[1], showdis);
-	echo_start();
-
-	while(1){
-		fscanf(stdin, "%c", &c);
-		fprintf(stdout,"the char you intput is %c\n", c);
-		switch(c){
-			case 'w':
-				state=FORWARD;
-				break;
-			case 's':
-				state=BACKOFF;
-				break;
-			case 'a':
-				state=TURNLEFT;
-				break;
-			case 'd':
-				state=TURNRIGHT;
-				break;
-			case 'p':
-				state=STOP;
-				break;
-			default:
-				break;
-		}
-		control_wheel(0,state);
-	}
 	return 1;
 }
